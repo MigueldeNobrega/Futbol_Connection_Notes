0227d506a0bcd36bce70b3809e65a26ad7c45480
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-02-10 18:38:40 +0100

    InitialCommit - LoginAndCreateAccount

---
+++ app/src/main/java/com/example/futbol_connection_notes/LoadScreenSplash.java
@@ -0,0 +1,32 @@
+package com.example.futbol_connection_notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoadScreenSplash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_screen_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if(currentUser==null){
+                    startActivity(new Intent(LoadScreenSplash.this,LoginActivity.class));
+                }else{
+                    startActivity(new Intent(LoadScreenSplash.this,MainActivity.class));
+                }
+                finish();
+            }
+        },1000);
+
+    }
+}
