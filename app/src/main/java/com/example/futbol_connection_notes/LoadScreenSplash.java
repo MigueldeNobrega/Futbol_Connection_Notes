4ecebf29ceb9a1a24353e35a77400d0515ef457f
MigueldeNobrega <miguel.eenobrega@gmail.com>
2025-02-14 18:52:07 +0100

    SplashActivityFinal

--- app/src/main/java/com/example/futbol_connection_notes/LoadScreenSplash.java
+++ app/src/main/java/com/example/futbol_connection_notes/LoadScreenSplash.java
@@ -15,18 +15,19 @@ public class LoadScreenSplash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable() { //Ejecuta la funcion 1seg después.
             @Override
             public void run() {
+                //Verifica si hay una sesion iniciada, si la hay entra en la mainActivity, si no, lleva al login.
                 FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
                 if(currentUser==null){
                     startActivity(new Intent(LoadScreenSplash.this,LoginActivity.class));
                 }else{
                     startActivity(new Intent(LoadScreenSplash.this,MainActivity.class));
                 }
-                finish();
+                finish();//Termina la actividad y no puede volver con botones de retroceso.
             }
-        },1000);
+        },1000); //1Seg
 
     }
 }
